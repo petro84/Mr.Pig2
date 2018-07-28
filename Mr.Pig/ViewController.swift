33805d71d679a45c474896c52eb9c2b26cc5b7b3
Larry Petroski <larry.petroski@yahoo.com>
2018-07-28 16:57:18 -0500

    Chapter 19 completed

--- Mr.Pig/ViewController.swift
+++ Mr.Pig/ViewController.swift
@@ -7,19 +7,96 @@
 //
 
 import UIKit
+import SceneKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
+    let game = GameHelper.sharedInstance
+    
+    var scnView: SCNView!
+    var gameScene: SCNScene!
+    var splashScene: SCNScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupScenes()
+        setupNodes()
+        setupActions()
+        setupTraffic()
+        setupGestures()
+        setupSounds()
+        
+        game.state = .tapToPlay
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override var prefersStatusBarHidden: Bool { return true }
+    override var shouldAutorotate: Bool { return false }
+    
+    func setupScenes() {
+        scnView = SCNView(frame: self.view.frame)
+        self.view.addSubview(scnView)
+        
+        gameScene = SCNScene(named: "/MrPig.scnassets/GameScene.scn")
+        splashScene = SCNScene(named: "/MrPig.scnassets/SplashScene.scn")
+        
+        scnView.scene = splashScene
+    }
+    
+    func setupNodes() {
+    }
+    
+    func setupActions() {
+    }
+    
+    func setupTraffic() {
+    }
+    
+    func setupGestures() {
+    }
+    
+    func setupSounds() {
+    }
+    
+    func startGame() {
+        splashScene.isPaused = true
+        
+        let transition = SKTransition.doorsOpenVertical(withDuration: 1.0)
+        
+        scnView.present(gameScene, with: transition, incomingPointOfView: nil, completionHandler: {
+            self.game.state = .playing
+            self.setupSounds()
+            self.gameScene.isPaused = false
+        })
+    }
+    
+    func stopGame() {
+        game.state = .gameOver
+        game.reset()
+    }
+    
+    func startSplash() {
+        gameScene.isPaused = true
+        
+        let transition = SKTransition.doorsOpenVertical(withDuration: 1.0)
+        scnView.present(splashScene, with: transition, incomingPointOfView: nil, completionHandler: {
+            self.game.state = .tapToPlay
+            self.setupSounds()
+            self.splashScene.isPaused = false
+            })
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if game.state == .tapToPlay {
+            startGame()
+        }
+    }
 }
 
